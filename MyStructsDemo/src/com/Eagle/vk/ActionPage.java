3f073ca8a4b66259e8c9867c160faa642180367a
vkalambe <vishalkalambe.cdac@gmail.com>
2016-11-24 01:06:52 +0530

    Add files via upload

---
+++ MyStructsDemo/src/com/Eagle/vk/ActionPage.java
@@ -0,0 +1,15 @@
+package com.Eagle.vk;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class ActionPage extends  ActionSupport  {
+
+	@Override
+	public String execute() throws Exception {
+		// TODO Auto-generated method stub
+		return "e";
+	}
+
+	
+	
+}
